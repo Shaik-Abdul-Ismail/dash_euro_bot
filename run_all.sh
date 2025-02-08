ed22cca75fe2e0b355a2e2cb10bddbdf7bd0e127
Shaik Abdul Ismail <133575906+Shaik-Abdul-Ismail@users.noreply.github.com>
2025-02-08 17:55:39 +0530

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,4 @@
 #python3 DASH_EURO_BOT.py
 #python3 POL_EUR_trallingloss.py
-python3 DASH_EURO_traillingloss.py
+#python3 DASH_EURO_traillingloss.py
+python3 DASH_EUR_traillingloss_multiple_orders.py
